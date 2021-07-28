c615351d3259a1fce1e4de54a99dee50bbab78b8
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2021-07-28 15:34:32 -0500

    Allow loading to fail

--- src/VMLinuxBindings.jl
+++ src/VMLinuxBindings.jl
@@ -2,17 +2,22 @@ module VMLinuxBindings
     using CBinding
 
     @static if Sys.islinux()
-        vmlinux_iob = IOBuffer()
-        run(pipeline(`bpftool btf dump file /sys/kernel/btf/vmlinux format c`; stdout=vmlinux_iob))
-        vmlinux_str = String(take!(vmlinux_iob))
+        try
+            vmlinux_iob = IOBuffer()
+            run(pipeline(`bpftool btf dump file /sys/kernel/btf/vmlinux format c`; stdout=vmlinux_iob))
+            vmlinux_str = String(take!(vmlinux_iob))
 
-        vmlinux_path = joinpath(@__DIR__, "vmlinux.h")
-        open(vmlinux_path, "w") do io
-            write(io, vmlinux_str)
-            flush(io)
+            vmlinux_path = joinpath(@__DIR__, "vmlinux.h")
+            open(vmlinux_path, "w") do io
+                write(io, vmlinux_str)
+                flush(io)
+            end
+
+            @eval c`-std=c99 -fparse-all-comments -I$(@__DIR__)`
+            @eval c"#include \"vmlinux.h\""
+        catch err
+            @warn "Failed to load VMLinux bindings!"
+            @debug "VMLinux error: $err"
         end
-
-        @eval c`-std=c99 -fparse-all-comments -I$(@__DIR__)`
-        @eval c"#include \"vmlinux.h\""
     end
 end
